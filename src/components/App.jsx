59cf3699e7a70eae6f3af2a27ab42c9743f590d5
sandrine <sandrinezocca@gmail.com>
2019-05-22 17:21:55 +0200

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  render() {
+    return (
+      <div>
+        <h3>Liste de courses</h3>
+        <div>En construction</div>
+      </div>
+    );
+  }
+}
+
+export default App;
