2fb93f0fbd17f8d69710a98a76df8d3af55500dc
sandrine <sandrinezocca@gmail.com>
2019-05-22 17:59:41 +0200

    modifier l'état du formulaire

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Form = () => {
-    return ( 
-        <div>Form component</div>
-     );
+class Form extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: '',
+			quantity: 0
+		}
+	}
+	render() {
+		return (
+			<div>
+				<h3>Ajouter des articles à acheter</h3>
+				<form>
+					<input type="number" placeholder="quantité" value={this.state.quantity} 
+					onChange={(event) => this.setState({quantity: event.target.value})}/>
+
+					<input type="text" placeholder="article" value={this.state.name} 
+					onChange={(event) => this.setState({name: event.target.value})}/>
+
+					<button type="submit">ajouter</button>
+				</form>
+			</div>
+		);
+	}
 }
- 
+
 export default Form;
