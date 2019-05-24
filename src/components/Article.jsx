478f874df8959079b16dd25275ba74d8d5581074
sandrine <sandrinezocca@gmail.com>
2019-05-24 14:59:04 +0200

    ajout d'un bouton de modif

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import '../style.css';
 
-const Article = (props) => {
-    return ( 
-        <div>
-            {props.data.quantity} {props.data.name}
-        </div>
-     );
+class Article extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isInEditMode: false
+        }
+    }
+
+    toggleEditMode = () => {
+        this.setState({ isInEditMode: !this.state.isInEditMode});
+    }
+
+
+    render() {
+        return (
+            <div>
+                <button className="btn btn-warning btn-xs edit" onClick={ this.toggleEditMode} >modif</button>
+                {this.props.data.quantity} {this.props.data.name}
+            </div>
+        );
+    }
 }
- 
+
 export default Article;
+
