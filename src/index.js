1016cfcaf10e7377a83d8c26e68de8af04ea824b
sandrine <sandrinezocca@gmail.com>
2019-05-24 12:57:41 +0200

    création du store Redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import  { createStore, combineReducers } from 'redux';
 
 import App from './components/App';
+
+// création du store
+
+const store = createStore();
 
 ReactDOM.render(<App />, document.getElementById('root'));
