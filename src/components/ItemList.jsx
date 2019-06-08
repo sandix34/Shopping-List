1431786cd2652fe31effeb844efb9472ea0bcd6d
sandrine <sandrinezocca@gmail.com>
2019-06-08 11:58:19 +0200

    correctif

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -4,6 +4,8 @@ import Article from './Article';
 // TODO: corriger ==> Uncaught TypeError: Cannot read property 'id' of undefined
 
 const ItemList = (props) => {
+    console.log(props);
+    
     return ( 
         <div>
             {props.articles.map(article => <Article data={article} key={article.id} editArticle={props.editArticle}/> )}
